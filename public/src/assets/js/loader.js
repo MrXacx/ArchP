34ed4c348b6f6b3fe94a183de4f701876fe1b4de
MrXacx <misterxacx@gmail.com>
2023-02-26 12:14:36 -0300

    add pattern js to html

---
+++ public/src/assets/js/loader.js
@@ -0,0 +1,6 @@
+function whileLoading(){
+    document.getElementsByTagName('body').style.display = "none";
+}
+function afterLoaded(){
+    document.getElementsByTagName('body').style.display = "flex";
+}
